28a9c7c6aaad34255f4d0b902de0a30275f801c1
abgodeghesi <arthur.godeghesi@gmail.com>
2021-03-30 01:32:21 -0300

    added play font

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import './App.css';
 import Main from './main';
 
 import Header from './components/Header'
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Play',
+      'Roboto',
+      'sans-serif'
+    ].join(','),
+  }
+});
+
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Main />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header/>
+        <Main />
+      </div>
+    </ThemeProvider>
   );
 }
 
