ffe680962561a13286500a996d7728d726aa8869
abgodeghesi <arthur.godeghesi@gmail.com>
2021-03-30 00:44:51 -0300

    012-schedule-refactor

--- src/pages/GameList.js
+++ src/pages/GameList.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import GameSlider from '../components/GameSlider';
 import Alert from '../components/Alert'
 
 import Games from '../mock/games';
 import TextMock from '../mock/texts';
 
+const useStyles = makeStyles({
+  alert:{
+    marginTop: 84,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+});
+
 export default function GameList() {
+  const classes = useStyles();
+
   return(
     <>
-      <div style={{ display: 'flex', justifyContent: 'center'}}>
+      <div className={classes.alert}>
         <Alert text={TextMock.MissingGames} />
       </div>
       <section>
--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,38 +1,72 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { Button, Box, Typography } from '@material-ui/core/';
+
+import TextMock from '../mock/texts';
+import Alert from '../components/Alert';
+
+import rocketIcon from '../assets/icons/ricket-icon.png';
+import tftIcon from '../assets/icons/tft-icon.png'
+import leagueIcon from '../assets/icons/lol-icon.png'
 
 const useStyles = makeStyles({
-  container: {
-    display: 'flex',
-    margin: 'auto',
-    flexDirection: 'column',
-  },
-  bannerContainer: {
-    padding: '8px',
-    alignContent: 'center',
-    backgroundColor: '#02173d',
-    color: 'white',
-    marginBottom: '8px',
-  },
-  cardsContainer: {
-    maxWidth: '100%',
-    flexDirection: 'row',
+  alert: {
     display: 'flex',
     justifyContent: 'center',
+    alignContent: 'center',
+    marginBottom: '12px',
   },
-  card: {
-
-    width: '300px',
-    height: '400px',
-    padding: '15px',
+  container: {
+    padding: 12,
+    backgroundColor: '#02173d',
+    borderRadius: 5,
+  },
+  cardRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  rocketContainer: {
     display: 'flex',
     flexDirection: 'column',
-    backgroundColor: '#02173d',
-    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: '280px',
+    padding: '12px',
+    background: 'linear-gradient(110deg, #062c7f 60%, #0a47cc 60%);',
     margin: '16px',
-    justifyContent: 'space-around',
+  },
+  tftContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    minWidth: '280px',
+    padding: '12px',
+    background: 'linear-gradient(110deg, #DF6F11 60%, #EC9144 60%);',
+    margin: '16px'
+    
+  },
+  leagueContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    minWidth: '280px',
+    padding: '12px',
+    background: 'linear-gradient(110deg, #0E5F7F 60%, #227EA3 60%);',
+    margin: '16px'
+  },
+  cardTitle: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '5px',
+  },
+  cardLogo: {
+    maxHeight: '110px',
+    marginBottom: '16px'
+  },
+  cardDetails: {
+    marginBottom: '8px',
+    marginTop: '8px',
   },
 });
 
@@ -41,40 +75,88 @@ export default function Schedule() {
 
   return (
     <>
-    <div className={classes.container}>
-      <div className={classes.bannerContainer}>
-        <Typography variant="h4"  align='center'>ESSA SEMANA NO TEAM HUB</Typography>
-      </div>
-      <div className={classes.cardsContainer}>
-        <div className={classes.card}>
-          <img alt='' style={{maxWidth: '220px'}} src={'https://www.nasef.org/media/2800/v_lockup_horizontal_pos_red.png'}/> 
-          <Typography variant="h9" align='center'>Sexta-feira 19/03<br />A partir das 20:00</Typography>
-          <Typography variant="h4" align='center'>5 X 5</Typography>
-          <Typography variant="h4" align='center'>VAGAS ILIMITADAS</Typography>
-          <Button variant="contained" color="primary">
-            Participar
-          </Button>
-        </div>
-        <div className={classes.card}>
-          <img alt='' style={{maxWidth: '220px'}} src={'https://logodownload.org/wp-content/uploads/2014/09/lol-league-of-Legends-logo.png'}/> 
-          <Typography variant="h9" align='center'>Sábado 20/03<br />A partir das 18:00</Typography>
-          <Typography variant="h4" align='center'>ARAM</Typography>
-          <Typography variant="h4" align='center'>4 VAGAS</Typography>
-          <Button variant="contained" color="primary">
-            Participar
-          </Button>
-        </div>
-        <div className={classes.card}>
-          <img alt='' style={{maxWidth: '220px'}} src={'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7c2daf28-6d53-4685-b5a8-6d44472af47e/d71eqkg-18444bae-57f5-40b6-b2a0-d218b6523bda.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi83YzJkYWYyOC02ZDUzLTQ2ODUtYjVhOC02ZDQ0NDcyYWY0N2UvZDcxZXFrZy0xODQ0NGJhZS01N2Y1LTQwYjYtYjJhMC1kMjE4YjY1MjNiZGEucG5nIn1dXX0.4Unn5RBBPxz0PMUDTZzBffwOjxfd_WU2ODBGX7JUri4'}/> 
-          <Typography variant="h9" align='center' gutterbottom={true}>Sábado 20/03<br />A partir das 20:00</Typography>
-          <Typography variant="h4" align='center'>Random Battles</Typography>
-          <Typography variant="h4" align='center' gutterbottom={true}>8 VAGAS</Typography>
-          <Button variant="contained" color="primary">
-            Participar
-          </Button>
-        </div>
-      </div>
+    <div className={classes.alert}>
+      <Alert text={TextMock.ScheduleDetails} />
     </div>
+      <main className={classes.container}>
+        <Typography variant="h3" align="center">
+          ESSA SEMANA NO TEAM HUB
+        </Typography>
+        <section className={classes.cardRow}>
+          <Box className={classes.rocketContainer} boxShadow={3}>
+            <div className={classes.cardTitle}>
+              <img className={classes.cardLogo} alt='' src={rocketIcon} />
+              <Typography variant="h5" align="center">
+                Rocket League
+              </Typography>
+            </div>
+            <Typography variant="h9" align="center">
+              <Box className={classes.cardDetails}>
+                Quinta-Feira 01/04<br/>A partir das 20:00
+              </Box>
+            </Typography>
+            <Typography variant="h5" align="center">
+              <Box className={classes.cardDetails}>
+                  3 vs 3
+              </Box>
+              <Box className={classes.cardDetails}>
+                  8 Vagas
+              </Box>
+            </Typography>
+            <Button variant="contained" color="primary" disableElevation>
+              Registrar
+            </Button>
+          </Box>
+          <Box className={classes.tftContainer} boxShadow={3}>
+            <div className={classes.cardTitle}>
+              <img className={classes.cardLogo} alt='' src={tftIcon} />
+              <Typography variant="h5" align="center">
+                TFT
+              </Typography>
+            </div>
+            <Typography variant="h9" align="center">
+              <Box className={classes.cardDetails}>
+                Sexta-Feira 02/04<br/>A partir das 18:00
+              </Box>
+            </Typography>
+            <Typography variant="h5" align="center">
+              <Box className={classes.cardDetails}>
+                  Pontos Corridos
+              </Box>
+              <Box className={classes.cardDetails}>
+                  16 Vagas
+              </Box>
+            </Typography>
+            <Button variant="contained" color="primary" disableElevation>
+              Registrar
+            </Button>
+          </Box>
+          <Box className={classes.leagueContainer} boxShadow={3}>
+            <div className={classes.cardTitle}>
+              <img className={classes.cardLogo} alt='' src={leagueIcon} />
+              <Typography variant="h5" align="center">
+                League of Legends
+              </Typography>
+            </div>
+            <Typography variant="h9" align="center">
+              <Box className={classes.cardDetails}>
+                Sábado 03/04<br/>A partir das 21:00
+              </Box>
+            </Typography>
+            <Typography variant="h5" align="center">
+              <Box className={classes.cardDetails}>
+                  5x5 da Zoeira
+              </Box>
+              <Box className={classes.cardDetails}>
+                  10 Vagas
+              </Box>
+            </Typography>
+            <Button variant="contained" color="primary" disableElevation>
+              Registrar
+            </Button>
+          </Box>
+        </section>
+      </main>
     </>
   );
 }
