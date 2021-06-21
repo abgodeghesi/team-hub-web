f4545346b5c99c827eba722ee6f3a78e9403e0a6
abgodeghesi <arthur.godeghesi@gmail.com>
2021-06-21 14:47:36 -0300

    removed card from schedule

--- src/mock/events.js
+++ src/mock/events.js
@@ -1,17 +1,3 @@
-import leagueIcon from '../assets/icons/lol-icon.png';
-
-const events = [
-  {
-    title: 'League 5v5',
-    day: 'quarta-feira 16/06',
-    hour: 'a partir das 20:00',
-    eventName: 'TH Major Gold #2',
-    vacancies: 'sem limite de',
-    bgColor: 'leagueBg',
-    icon: leagueIcon,
-    button: 'default',
-    callToAction: 'https://forms.gle/6u17zpFG62oYCXGy8',
-  },
-];
+const events = [];
 
 export default events;
