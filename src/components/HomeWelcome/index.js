2de738e4de07a9458b59e412dacb07f0398c2796
abgodeghesi <arthur.godeghesi@gmail.com>
2021-07-22 21:19:55 -0300

    feat(bg): created a random bg feature

--- src/components/HomeWelcome/index.js
+++ src/components/HomeWelcome/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Freljord from '../../assets/freljord.jpg';
+import Dust2 from '../../assets/bg-dust2.jpg';
+import Rocket from '../../assets/rocket-wallpaper.jpg';
 
 const Main = styled.main`
   width: 100%;
 `;
 
 const Background = styled.section`
-  background-image: url(${Freljord});
   background-repeat: no-repeat;
   background-size: cover;
-  min-height: 90vh;
+  min-height: 100vh;
 `;
 
 const TitleDiv = styled.div`
@@ -20,7 +21,7 @@ const TitleDiv = styled.div`
   height: 90vh;
   line-height: 120px;
   justify-content: center;
-  padding: 0 8px;
+  padding: 0 8px 0;
   @media (min-width: 560px) {
     padding: 0 0;
   }
@@ -28,49 +29,63 @@ const TitleDiv = styled.div`
 
 const H1 = styled.h1`
   padding: 0;
-  margin: 0 auto;
   font-family: 'Teko', sans-serif;
   color: #f5f5f5;
   font-size: 2rem;
   font-weight: 500;
+  padding: 8px 0 0;
+  @media (min-width: 560px) {
+    text-align: center;
+  }
 `;
 
 const H2 = styled.h2`
   padding: 0;
-  margin: -30px auto 0;
+  margin: -60px 0 0;
   font-family: 'Teko', sans-serif;
   color: #f5f5f5;
   font-size: 9rem;
+  padding: 8px 0 0;
+  @media (min-width: 560px) {
+    text-align: center;
+  }
 `;
 
 const H3 = styled.h3`
   padding: 0;
   margin: 0 auto;
   color: #f5f5f5;
+  max-width: 60ch;
   font-size: 1rem;
   font-weight: 400;
-  text-align: center;
   line-height: 1.5;
+  padding: 8px 0 0;
+  @media (min-width: 560px) {
+    text-align: center;
+    text-shadow: 1px 1px black;
+  }
 `;
 
-const HomeWelcome = () => (
-  <Main>
-    <Background>
-      <TitleDiv>
-        <H1>bem-vindo(a) ao</H1>
-        <H2>TEAM HUB</H2>
-        <H3>
-          Sua plataforma virtual de torneios online, com experiência única,
-          focada
-          <br />
-          especialmente nos jogadores. Desfrute de transmissões ao vivo, eventos
-          dinâmicos,
-          <br />
-          competitivos e for fun, tudo no mesmo lugar.
-        </H3>
-      </TitleDiv>
-    </Background>
-  </Main>
-);
+const HomeWelcome = () => {
+  const bgs = [Freljord, Rocket, Dust2];
+
+  const selectBG = bgs[Math.floor(Math.random() * bgs.length)];
+
+  return (
+    <Main>
+      <Background style={{ backgroundImage: `url(${selectBG})` }}>
+        <TitleDiv>
+          <H1>bem-vindo(a) ao</H1>
+          <H2>TEAM HUB</H2>
+          <H3>
+            Sua plataforma de e-Sports online com experiência única, focada
+            especialmente nos jogadores. Desfrute de transmissões ao vivo,
+            eventos dinâmicos, competitivos e for fun, tudo no mesmo lugar.
+          </H3>
+        </TitleDiv>
+      </Background>
+    </Main>
+  );
+};
 
 export default HomeWelcome;
