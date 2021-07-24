1ab2ab0ab1246484c87ced823b3683c9decc24e9
abgodeghesi <arthur.godeghesi@gmail.com>
2021-07-23 22:11:15 -0300

    fix(styles): fixed several styles for mobile and desktop

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -29,43 +29,49 @@ const FooterText = styled.h1`
 `;
 const SocialContainer = styled.section`
   margin: 0 auto;
-  padding: 24px 0 0;
+  padding: 16px 0 0;
   display: flex;
 `;
-const SocialIcons = styled.div`
-  fill: #f5f5f5;
+
+const IconsDiv = styled.div`
   background-color: #35ff69;
   border-radius: 50%;
   cursor: pointer;
   padding: 8px;
   margin: 0 8px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+`;
+const SocialIcons = styled.div`
+  margin: auto;
 `;
 
 const Footer = () => {
   const socialMedia = [
     {
       title: 'discord',
-      icon: <SiDiscord size="24" />,
+      icon: <SiDiscord size="24" fill="#00091a" />,
       link: 'https://discord.gg/dfpM4Cj3gb',
     },
     {
       title: 'twitch',
-      icon: <SiTwitch size="24" />,
+      icon: <SiTwitch size="24" fill="#00091a" />,
       link: 'https://www.twitch.tv/kevinhocn',
     },
     {
       title: 'youtube',
-      icon: <SiYoutube size="24" />,
+      icon: <SiYoutube size="24" fill="#00091a" />,
       link: 'https://www.youtube.com/channel/UCvVgDhL3r2If2CxdnJcaiwQ',
     },
     {
       title: 'instagram',
-      icon: <SiInstagram size="24" />,
+      icon: <SiInstagram size="24" fill="#00091a" />,
       link: '',
     },
     {
       title: 'github',
-      icon: <SiGithub size="24" />,
+      icon: <SiGithub size="24" fill="#00091a" />,
       link: 'https://github.com/abgodeghesi/team-hub-web',
     },
   ];
@@ -77,14 +83,16 @@ const Footer = () => {
       </FooterText>
       <SocialContainer>
         {socialMedia.map(item => (
-          <SocialIcons
-            alt={item.title}
-            onClick={() => {
-              window.open(item.link);
-            }}
-          >
-            {item.icon}
-          </SocialIcons>
+          <IconsDiv>
+            <SocialIcons
+              alt={item.title}
+              onClick={() => {
+                window.open(item.link);
+              }}
+            >
+              {item.icon}
+            </SocialIcons>
+          </IconsDiv>
         ))}
       </SocialContainer>
     </FooterContainer>
